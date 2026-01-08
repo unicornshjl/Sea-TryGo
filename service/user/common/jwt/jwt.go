7765c9a654a9d1e6525f8478213a737a53599347
unicornshjl <3159669749@qq.com>
2026-01-08 12:00:58 +0800

    新增生成 json web token 的文件

---
+++ service/user/common/jwt/jwt.go
@@ -0,0 +1,15 @@
+package jwt
+
+import (
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func GetToken(secretKey string, iat, seconds int64, userId int64) (string, error) {
+	claims := make(jwt.MapClaims)
+	claims["exp"] = iat + seconds
+	claims["iat"] = iat
+	claims["userId"] = userId
+	token := jwt.New(jwt.SigningMethodHS256)
+	token.Claims = claims
+	return token.SignedString([]byte(secretKey))
+}
