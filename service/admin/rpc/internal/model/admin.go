067acce929f284dd086b332e8b0579feab09e258
unicornshjl <3159669749@qq.com>
2026-01-15 16:38:23 +0800

    fix(user,admin):将model归属到rpc中

--- service/admin/rpc/internal/model/admin.go
+++ service/admin/rpc/internal/model/admin.go
@@ -2,12 +2,27 @@ package model
 
 import "time"
 
+type Admin struct {
+	Id         uint64            `gorm:"primaryKey"`
+	Username   string            `gorm:"column:username"`
+	Password   string            `gorm:"column:password"`
+	Email      string            `gorm:"column:email"`
+	ExtraInfo  map[string]string `gorm:"column:extra_info;serializer:json"`
+	CreateTime time.Time         `gorm:"column:create_time;autoCreateTime"`
+	UpdateTime time.Time         `gorm:"column:update_time;autoUpdateTime"`
+}
+
+func (Admin) TableName() string {
+	return "admins"
+}
+
 type User struct {
 	Id         uint64            `gorm:"primaryKey"`
 	Username   string            `gorm:"column:username"`
 	Password   string            `gorm:"column:password"`
 	Email      string            `gorm:"column:email"`
 	Status     int64             `gorm:"column:status;default:0"`
+	Score      uint32            `gorm:"colum:score"`
 	ExtraInfo  map[string]string `gorm:"column:extra_info;serializer:json"`
 	CreateTime time.Time         `gorm:"column:create_time;autoCreateTime"`
 	UpdateTime time.Time         `gorm:"column:update_time;autoUpdateTime"`
--- service/user/rpc/internal/model/user.go
+++ service/user/rpc/internal/model/user.go
@@ -2,26 +2,13 @@ package model
 
 import "time"
 
-type Admin struct {
-	Id         uint64            `gorm:"primaryKey"`
-	Username   string            `gorm:"column:username"`
-	Password   string            `gorm:"column:password"`
-	Email      string            `gorm:"column:email"`
-	ExtraInfo  map[string]string `gorm:"column:extra_info;serializer:json"`
-	CreateTime time.Time         `gorm:"column:create_time;autoCreateTime"`
-	UpdateTime time.Time         `gorm:"column:update_time;autoUpdateTime"`
-}
-
-func (Admin) TableName() string {
-	return "admins"
-}
-
 type User struct {
 	Id         uint64            `gorm:"primaryKey"`
 	Username   string            `gorm:"column:username"`
 	Password   string            `gorm:"column:password"`
 	Email      string            `gorm:"column:email"`
 	Status     int64             `gorm:"column:status;default:0"`
+	Score      int32             `gorm:"column:score"`
 	ExtraInfo  map[string]string `gorm:"column:extra_info;serializer:json"`
 	CreateTime time.Time         `gorm:"column:create_time;autoCreateTime"`
 	UpdateTime time.Time         `gorm:"column:update_time;autoUpdateTime"`
