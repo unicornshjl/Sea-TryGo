8ab60f4f4798bfaadc772c304953bd9da67f753e
unicornshjl <3159669749@qq.com>
2026-01-22 22:25:57 +0800

    fix(admin):将数据库操作分离，从rpc层移动到model层

--- service/admin/rpc/internal/model/admin.go
+++ service/admin/rpc/internal/model/admin.go
@@ -1,33 +1,120 @@
 package model
 
-import "time"
+import (
+	"context"
 
-type Admin struct {
-	Id         uint64            `gorm:"primaryKey"`
-	Username   string            `gorm:"column:username"`
-	Password   string            `gorm:"column:password"`
-	Email      string            `gorm:"column:email"`
-	ExtraInfo  map[string]string `gorm:"column:extra_info;serializer:json"`
-	CreateTime time.Time         `gorm:"column:create_time;autoCreateTime"`
-	UpdateTime time.Time         `gorm:"column:update_time;autoUpdateTime"`
+	"gorm.io/gorm"
+)
+
+type AdminModel struct {
+	conn *gorm.DB
 }
 
-func (Admin) TableName() string {
-	return "admins"
+func NewAdminModel(db *gorm.DB) *AdminModel {
+	return &AdminModel{
+		conn: db,
+	}
 }
 
-type User struct {
-	Id         uint64            `gorm:"primaryKey"`
-	Username   string            `gorm:"column:username"`
-	Password   string            `gorm:"column:password"`
-	Email      string            `gorm:"column:email"`
-	Status     int64             `gorm:"column:status;default:0"`
-	Score      uint32            `gorm:"colum:score"`
-	ExtraInfo  map[string]string `gorm:"column:extra_info;serializer:json"`
-	CreateTime time.Time         `gorm:"column:create_time;autoCreateTime"`
-	UpdateTime time.Time         `gorm:"column:update_time;autoUpdateTime"`
+func (m *AdminModel) FindOneUserByUid(ctx context.Context, uid int64) (*User, error) {
+	var user User
+	err := m.conn.WithContext(ctx).Where("uid = ?", uid).First(&user).Error
+	if err == nil {
+		return &user, nil
+	}
+	if err == gorm.ErrRecordNotFound {
+		return nil, ErrorNotFound
+	}
+	return nil, err
 }
 
-func (User) TableName() string {
-	return "users"
+func (m *AdminModel) FindOneUserByUsername(ctx context.Context, username string) (*User, error) {
+	var user User
+	err := m.conn.WithContext(ctx).Where("username = ?", username).First(&user).Error
+	if err == nil {
+		return &user, nil
+	}
+	if err == gorm.ErrRecordNotFound {
+		return nil, ErrorNotFound
+	}
+	return nil, err
+}
+
+func (m *AdminModel) DeleteOneUserByUid(ctx context.Context, uid int64) error {
+	result := m.conn.WithContext(ctx).Where("uid = ?", uid).Delete(&User{})
+	if result.RowsAffected == 0 {
+		return ErrorNotFound
+	}
+	return result.Error
+}
+
+func (m *AdminModel) UpdateOneUserByUid(ctx context.Context, uid int64, newUser *User) error {
+	err := m.conn.WithContext(ctx).Model(&User{}).Where("uid = ?", uid).Updates(newUser).Error
+	return err
+}
+
+func (m *AdminModel) FindOneAdminByUid(ctx context.Context, uid int64) (*Admin, error) {
+	var admin Admin
+	err := m.conn.WithContext(ctx).Where("uid = ?", uid).First(&admin).Error
+	if err == nil {
+		return &admin, nil
+	}
+	if err == gorm.ErrRecordNotFound {
+		return nil, ErrorNotFound
+	}
+	return nil, err
+}
+
+func (m *AdminModel) FindOneAdminByUsername(ctx context.Context, username string) (*Admin, error) {
+	var admin Admin
+	err := m.conn.WithContext(ctx).Where("username = ?", username).First(&admin).Error
+	if err == nil {
+		return &admin, nil
+	}
+	if err == ErrorNotFound {
+		return nil, ErrorNotFound
+	}
+	return nil, err
+}
+
+func (m *AdminModel) UpdateOneAdminByUid(ctx context.Context, uid int64, newAdmin *Admin) error {
+	err := m.conn.WithContext(ctx).Model(&Admin{}).Where("uid = ?", uid).Updates(newAdmin).Error
+	return err
+}
+
+func (m *AdminModel) UpdateUserStatusByUid(ctx context.Context, uid int64, status int64) error {
+	result := m.conn.WithContext(ctx).Model(&User{}).Where("uid = ?", uid).Update("status", status)
+	if result.RowsAffected == 0 {
+		return ErrorNotFound
+	}
+	return result.Error
+}
+
+func (m *AdminModel) UpdateUserPasswordByUid(ctx context.Context, uid int64, newPassword string) error {
+	err := m.conn.WithContext(ctx).Model(&User{}).Where("uid = ?", uid).Update("password", newPassword).Error
+	return err
+}
+
+func (m *AdminModel) InsertOneAdmin(ctx context.Context, admin *Admin) error {
+	err := m.conn.WithContext(ctx).Create(admin).Error
+	return err
+}
+
+func (m *AdminModel) FindUserListByKeyword(ctx context.Context, page int64, pageSize int64, keyword string) ([]*User, int64, error) {
+	var users []*User
+	var total int64
+	db := m.conn.WithContext(ctx).Model(&User{})
+	if len(keyword) > 0 {
+		keyWord := "%" + keyword + "%"
+		db = db.Where("username LIKE ? OR email LIKE ?", keyWord, keyWord)
+	}
+	if err := db.Count(&total).Error; err != nil {
+		return nil, 0, err
+	}
+	offset := (page - 1) * pageSize
+	err := db.Order("id desc").Offset(int(offset)).Limit(int(pageSize)).Find(&users).Error
+	if err != nil {
+		return nil, 0, err
+	}
+	return users, total, nil
 }
